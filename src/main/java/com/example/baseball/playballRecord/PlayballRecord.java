0244c2973f37ec21a367466ef15b94d0185783ec
anjaehun <juhun104@naver.com>
2023-08-21 15:31:07 +0900

    [FETCH]각종 entity 추가 초안(경기 기록까지)

--- src/main/java/com/example/baseball/playballRecord/PlayballRecord.java
+++ src/main/java/com/example/baseball/playballRecord/PlayballRecord.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "playballRecord")
+@Table(name = "playBallRecord")
 public class PlayballRecord {
 
     @Id
