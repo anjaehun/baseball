dee27cc9da9140fd98498fbcc4c22b27e13dcc93
anjaehun <juhun104@naver.com>
2023-09-05 02:26:00 +0900

    feat. error 수정 -> 한 사람이 여러 팀을 등록할 때 unique6 있어서 에러 발생 ->  findByNickname -> findByTeamAndTeamName 으로 수정

--- src/main/java/com/example/baseball/team/service/TeamService.java
+++ src/main/java/com/example/baseball/team/service/TeamService.java
@@ -9,6 +9,8 @@ import com.example.baseball.team.entity.TeamEntity;
 import com.example.baseball.team.exception.NoTeamByOneException;
 import com.example.baseball.team.exception.SameTeamNameException;
 import com.example.baseball.team.repository.TeamRepository;
+import com.example.baseball.team.response.TeamListShowByTeamIdResponse;
+import com.example.baseball.team.response.TeamListShowResponse;
 import com.example.baseball.teamMember.entity.TeamMemberEntity;
 import com.example.baseball.teamMember.enumType.TeamFounderAcceptRole;
 import com.example.baseball.teamMember.repository.TeamMemberRepository;
@@ -16,12 +18,15 @@ import com.example.baseball.teamPerformance.TeamPerformanceEntity;
 import com.example.baseball.teamPerformance.repository.TeamPerformanceRepository;
 import com.example.baseball.user.entity.UserEntity;
 import com.example.baseball.user.repository.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,15 +42,18 @@ public class TeamService {
 
     private final TeamPerformanceRepository teamPerformanceRepository;
 
+    private final ObjectMapper objectMapper;
 
 
-    public TeamService(UserRepository userRepository, TeamRepository teamRepository, TeamMemberRepository teamMemberRepository, PitcherRecordRepository pitcherRecordRepository, HitterRecordRepository hitterRecordRepository, TeamPerformanceRepository teamPerformanceRepository) {
+
+    public TeamService(UserRepository userRepository, TeamRepository teamRepository, TeamMemberRepository teamMemberRepository, PitcherRecordRepository pitcherRecordRepository, HitterRecordRepository hitterRecordRepository, TeamPerformanceRepository teamPerformanceRepository, ObjectMapper objectMapper) {
         this.userRepository = userRepository;
         this.teamRepository = teamRepository;
         this.teamMemberRepository = teamMemberRepository;
         this.pitcherRecordRepository = pitcherRecordRepository;
         this.hitterRecordRepository = hitterRecordRepository;
         this.teamPerformanceRepository = teamPerformanceRepository;
+        this.objectMapper = objectMapper;
     }
 
     /**
@@ -90,7 +98,6 @@ public class TeamService {
             email = userDetails.getUsername(); // 사용자 이메일 정보를 추출
         }
 
-        // System.out.println("email: " + email);
 
         String name = "기본 이름";
         Optional<UserEntity> existingEmail = userRepository.findByEmail(email);
@@ -103,22 +110,25 @@ public class TeamService {
 
         return name;
     }
-    public TeamEntity postTeam(TeamPostRequest request) throws SameTeamNameException, NoTeamByOneException {
+    public TeamEntity postTeam(TeamPostRequest request) throws SameTeamNameException, NoTeamByOneException, JsonProcessingException {
 
         String name = username();
         String nickname = userNickname();
         String mainCoach = request.getMainCoach();
+        String teamName = request.getTeamName();
 
         LocalDateTime currentTime = LocalDateTime.now();
 
-        Optional<UserEntity> existingEmail = userRepository.findByNickname(nickname);
-        Optional<TeamEntity> existingTeamName = teamRepository.findByTeamName(request.getTeamName());
+        // 중복된 팀 이름을 확인
+        boolean isTeamNameUnique = !teamRepository.existsByTeamName(teamName);
 
-
-        if (existingTeamName.isPresent()) {
+        if (!isTeamNameUnique) {
             throw new SameTeamNameException("팀의 이름이 중복되었습니다. 다시 시도해 주세요");
         }
+
+        Optional<UserEntity> existingEmail = userRepository.findByNickname(nickname);
         UserEntity user;
+
         if (existingEmail.isPresent()) {
             user = existingEmail.get();
         } else {
@@ -126,26 +136,26 @@ public class TeamService {
             user = null; // 또는 다른 값으로 설정
         }
 
-        if(request.getMainCoach().equals("미정")){
+        if ("미정".equals(request.getMainCoach())) {
             mainCoach = "공석";
         }
 
         var team = TeamEntity.builder()
                 .masterName(name)
                 .masterNickname(nickname)
-                .teamName(request.getTeamName())
+                .teamName(teamName)
                 .mainCoach(mainCoach)
                 .teamImg(request.getTeamImg())
                 .registerDt(currentTime)
                 .id(user)
-              .build();
+                .build();
 
         teamRepository.save(team);
 
         // 창설자 팀 멤버 등록
 
         // teamName -> 예외처리로 인해 중복된 팀 네임은 해결 될 수 있음
-        TeamEntity findByTeamName = teamRepository.findByTeamName(request.getTeamName())
+        TeamEntity findByTeamName = (TeamEntity) teamRepository.findByTeamName(teamName)
                 .orElseThrow(() -> new NoTeamByOneException("팀정보가 없습니다"));
 
         var teamPerfomance = TeamPerformanceEntity.builder()
@@ -173,25 +183,25 @@ public class TeamService {
 
         teamMemberRepository.save(teamMember);
 
-        TeamMemberEntity teamMemberByNickname = (TeamMemberEntity) teamMemberRepository.findByNickname(nickname)
+        TeamMemberEntity teamMemberByTeamNickname = (TeamMemberEntity) teamMemberRepository.findByTeamAndNickname(findByTeamName,userNickname())
                 .orElseThrow(() -> new NoTeamByOneException("팀멤버가 없습니다"));
 
         var pitcherRecord = PitcherRecordEntity.builder()
-                .teamName(team.getTeamName())
-                .name(teamMemberByNickname.getName())
-                .nickname(teamMemberByNickname.getNickname())
+                .teamName(teamName)
+                .name(name)
+                .nickname(nickname)
                 .atBat(0.0).hit(0.0).doubleHit(0.0).tripleHit(0.0).homeRun(0.0).strikeout(0.0).unintentionalWalk(0.0).intentionalWalk(0.0).runsAllowed(0.0).earnedRun(0.0).earnedRunAverage(0.0).whip(0.0).strikeoutPercent(0.0)
-                .team(team)
-                .teamMember(teamMemberByNickname)
+                .team(findByTeamName)
+                .teamMember(teamMemberByTeamNickname)
                 .build();
 
         var hitterRecord = HitterRecordEntity.builder()
-                .teamName(team.getTeamName())
-                .name(teamMemberByNickname.getName())
-                .nickname(teamMemberByNickname.getNickname())
+                .teamName(teamName)
+                .name(name)
+                .nickname(nickname)
                 .atBat(0.0).hit(0.0).doubleHit(0.0).tripleHit(0.0).homeRun(0.0).unintentionalWalk(0.0).intentionalWalk(0.0).hitByPitch(0.0).runsBattedIn(0.0).runs(0.0).stolenBases(0.0).attempts(0.0).strikeout(0.0).battingAverage(0.0).sluggingPercentage(0.0).onBasePercentage(0.0).stolenBaseSuccessRate(0.0)
-                .team(team)
-                .teamMember(teamMemberByNickname)
+                .team(findByTeamName)
+                .teamMember(teamMemberByTeamNickname)
                 .build();
 
         pitcherRecordRepository.save(pitcherRecord);
@@ -201,22 +211,46 @@ public class TeamService {
     }
 
 
-    public List<TeamEntity> getAllTeamList() {
+
+    public List<TeamListShowResponse> getAllTeamList() {
         List<TeamEntity> teamList = teamRepository.findAll();
-        return teamList;
+
+        List<TeamListShowResponse> teamListShowResponse = new ArrayList<>();
+
+        for(TeamEntity teamEntityes :  teamList ){
+            TeamListShowResponse response = new TeamListShowResponse();
+            response.setTeamId(teamEntityes.getTeamId());
+            response.setTeamName(teamEntityes.getTeamName());
+            response.setTeamDescription(teamEntityes.getTeamDescription());
+            response.setTeamImg(teamEntityes.getTeamImg());
+
+            teamListShowResponse.add(response);
+        }
+        return teamListShowResponse;
 
     }
 
-    public TeamEntity getTeamByTeamId(Integer teamId) throws NoTeamByOneException {
+    public TeamListShowByTeamIdResponse getTeamByTeamId(Integer teamId) throws NoTeamByOneException {
         Optional<TeamEntity> optionalTeam = teamRepository.findById(teamId);
-        System.out.println(optionalTeam);
-        
 
-        if (optionalTeam.isPresent()) {
-            return optionalTeam.get();
-        } else {
-            throw new NoTeamByOneException("팀정보가 없습니다");
+        if(!(optionalTeam.isPresent())){
+            throw new NoTeamByOneException("팀 정보가 없습니다. ");
         }
+
+        TeamListShowByTeamIdResponse response =
+                TeamListShowByTeamIdResponse.builder()
+                        .teamId(optionalTeam.get().getTeamId())
+                        .teamName(optionalTeam.get().getTeamName())
+                        .mainCoach(optionalTeam.get().getMainCoach())
+                        .teamDescription(optionalTeam.get().getTeamDescription())
+                        .teamLogoImage(optionalTeam.get().getTeamLogoImage())
+                        .teamImg(optionalTeam.get().getTeamImg())
+                        .build();
+
+
+
+            return response;
+
     }
 
 
